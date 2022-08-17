ca692fde01382c40aecb25275adad4758e8bfb8a
JQSELMAQ <109808441+JQSELMAQ@users.noreply.github.com>
2022-08-17 09:47:17 -0400

    Update robot.py
    Added comments to various lines

--- robot.py
+++ robot.py
@@ -62,7 +62,8 @@ def deletemode(directory):
         if "impaired_" in items:
             os.system(f'rm {items}')
 
-
+#Delete mode is an optional parameter for running the file download. If selected, it'll just delete anything with "impaired_" in the name. 
+#Replace mode, which is the alternative, and the default, only removes files in the event a file with that exact name already exists. 
 
 
 def fetch_steps(id):
@@ -83,10 +84,7 @@ def fetch_steps(id):
     else:
         for step in experiment['steps']:
             yield (step['audio'], step.get('correct_answer', None))
-print("Function called.")
-
-
-
+#This function collects the list of audio files and their corresponding correct answer strings based on the provided experiment ID.
 
 def download_func(directory):
     global iterate, compute_num
@@ -94,6 +92,7 @@ def download_func(directory):
     if "D" in x:
         deletemode(directory)
         listed = os.listdir(directory)
+        #turns directory into an iterable list.
     try:
 
         for url, correct_answer in fetch_steps(exper_id):
@@ -106,35 +105,34 @@ def download_func(directory):
 
             wget.download(shorthand, out=directory)
             print(" Downloading:", shorthand)
-            # download file off the web, map it to specified filepath
+            # download file off the web, route the download to given filepath
             pos = shorthand.find("impaired_")
             cut = shorthand[pos:]
+            #find filename
             wavetype = cut + ".wav"
+            #add the .wav extension to allow it to be recognized by the file system.
 
             if truncated:
                 new_dictionary[f"cut_{iterate}_{wavetype}"] = iterate
+                #This is how we calculate the file index that we later return to the testbed.
             else:
                 new_dictionary[wavetype] = iterate
             os.rename(f"{directory}/{cut}", f"{directory}/{wavetype}")
+            #rename file without extension to add the extension
             os.rename(f"{directory}/{wavetype}", f"{directory}/{iterate}_{wavetype}")
-            print(f"Renamed {cut} to {iterate}_{wavetype}")
             trimmed = AudioSegment.from_file(f"{directory}/{iterate}_{wavetype}")
             trimmed_removed = trimmed[1500:]
             trimmed_removed.export(out_f=f"{directory}/cut_{iterate}_{wavetype}",
-                                       format="wav")
+                                   format="wav")
             trim_dict[wavetype] = f"cut_{iterate}_{wavetype}"
+            #Trims the first 1500ms of every recording and stores it in the directory as cut_#_impaired_...
             iterate += 1
             compute_num += 1
     except FileExistsError:
         print("Duplicate file found. Replacing...")
         os.remove(directory + "/" + listed[iterate])
         print(count, "duplicate files found and replaced")
-
-        # convert to wav file
-        # rename file
-
-
-
+        #Replace mode operation
 iterate = 0
 
 
@@ -144,8 +142,10 @@ def encode_func(directory, waves):
         if "cut_" in waves:
             try:
                 beginencode = open(f"{folder}/{waves}", 'rb')
+                #If temporary file is created this will work.
             except FileNotFoundError:
                 beginencode = open(waves, 'rb')
+                #If you opt to work out of the current directory the previous line throws a FileNotFoundError.
 
 
             # open recording, parameter = read bytes
@@ -153,6 +153,7 @@ def encode_func(directory, waves):
             decode = beginencode.read()
             # read recording
             convert = base64.b64encode(decode).decode('utf-8')
+            #encoding
             try:
                 encoded_files[waves] = convert
             except IndexError:
@@ -172,8 +173,10 @@ def eval_phase(text, cur_file):
     global itervar
     print(f"File index: {new_dictionary[cur_file]}, Response: {text}\n")
     return evaluate(text)
-
-# shutil.copyfile(items, str(iterate) + "_" + items)
+    #There was a little slip up, I was asked to "modularize" the program, but the intended meaning was to separate everything into functions.
+    #Evaluate was once an integrated function, turned into a module.
+    #It simply serves to take a given string output and convert it to a license plate format.
+    #E.g. Mike Tango Two Zero 7 0 -> MT2070
 
 
 def google_sendoff(lists, cur_item):
@@ -194,16 +197,16 @@ def google_sendoff(lists, cur_item):
             "phrases": [nato_list],
             "boost": 100
         }]
-
+    #JSON document to form google request.
     request = requests.post(API_URL, json=config)
-
+    #Send request
     data = request.json()
+    #Read in response object as a JSON object, which, it already was.
     try:
         plaintext = data['results'][0]['alternatives'][0]['transcript']
         print("---------------------------------")
         return plaintext
 
-    # print(items, str(iterate)+"_"+items)
     except KeyError:
         # If you chose to download the files to a current directory,
         # chances are there'll be a non .wav file in there somewhere.
@@ -225,6 +228,7 @@ def api_submission(web_address, identification, file_index, response):
                             )
     json_obj = sendreq.json()
     print(json_obj['steps'])
+    #This function, as the name implies,  returns the file index and speech to text response to the testbed.
 
 
 def mainfunction():
@@ -285,6 +289,7 @@ def mainfunction():
                 poskey = items.find("impaired_")
                 poskeyend = items.find(".wav")
                 lev_dist = lev(compressed_str, answer_dict[items[poskey:poskeyend]], True)
+                #Run levenshtein distance algorithm on Google's output vs correct answer.
                 result_dict[items] = (g_interpret, compressed_str, answer_dict[items[poskey:poskeyend]], lev_dist)
                 print(f"Levenshtein distance is: {lev_dist}")
                 iterate += 1
@@ -295,7 +300,7 @@ mainfunction()
 
 os.system(f"rmdir {folder}")
 
-print("Printing result dictionary in 3.")
+'''print("Printing result dictionary in 3.")
 time.sleep(0.5)
 print("2")
 time.sleep(0.5)
@@ -305,4 +310,4 @@ with open("results.json", 'w') as blank:
     json.dump(result_dict, blank)
     blank.close()
 
-# verify this is the same dictionary structure as result_dict
+# verify this is the same dictionary structure as result_dict'''
